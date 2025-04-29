d8ef5871f56bee44b47515637f7f23e2b8ebde46
lucasga3 <lucasga3@al.insper.edu.br>
2025-04-29 09:15:10 -0300

    ex2-v1

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,16 @@ def rolar_dados(numero):
        lista.append(random.randint(1,6))
   
    return lista
+
+def guardar_dado (dados_rolados, dados_guardados, guardar):
+    novo = []
+    guardados = []
+
+    for i in range (len(dados_guardados)):
+        guardados.append(dados_guardados[i])
+    
+    guardados.append(dados_rolados[guardar])
+
+    novo = (dados_guardados,guardados)
+
+    return novo
