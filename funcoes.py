4f4a8bfab7480af5ef7d9bfd6c6d0b716b84375d
lucasga3 <lucasga3@al.insper.edu.br>
2025-04-30 19:56:03 -0300

    ex11-v1

--- funcoes.py
+++ funcoes.py
@@ -111,3 +111,15 @@ def calcula_pontos_quina (faces):
             resultado += 50
             return resultado
     return 0
+
+def calcula_pontos_regra_avancada (faces):
+
+    dicio = {
+        'cinco_iguais': calcula_pontos_quina(faces),
+        'full_house': calcula_pontos_full_house(faces),
+        'quadra': calcula_pontos_quadra(faces),
+        'sem_combinacao': calcula_pontos_soma(faces),
+        'sequencia_alta': calcula_pontos_sequencia_alta(faces),
+        'sequencia_baixa': calcula_pontos_sequencia_baixa(faces)
+    }
+    return dicio
