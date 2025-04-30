3a4efc5102012b5453c4092eb41c98d73ff08233
lucasga3 <lucasga3@al.insper.edu.br>
2025-04-30 08:49:22 -0300

    ex6-v1

--- funcoes.py
+++ funcoes.py
@@ -50,3 +50,12 @@ def calcula_pontos_soma (faces):
         resultado += faces[i]
     
     return resultado
+
+def calcula_pontos_sequencia_baixa (faces):
+    if 1 in faces and 2 in faces and 3 in faces and 4 in faces:
+        return 15
+    if 2 in faces and 3 in faces and 4 in faces and 5 in faces:
+        return 15
+    if 3 in faces and 4 in faces and 5 in faces and 6 in faces:
+        return 15
+    return 0
